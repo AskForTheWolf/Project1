05cd8b8ebf26c54c18412a740e54c0eb2dde7343
Curry Lee <ej20curry@gmail.com>
2017-05-17 12:45:03 -0400

    it lights up now

---
+++ script.js
@@ -0,0 +1,94 @@
+//round #
+//data-quad array to keep track of what is lit
+//data-quad[0] = null, 1 = red ETC... MAYBE? WHY DO I NEED AN ARRAY?
+//CAN THIS JUST BE HANDLED BY ATTR DATA QUAD?
+//LET'S INCLUDE BOTH WAYS, ASK ABOUT THIS TOMORROW AM
+//BUT I THINK I NEED THE ARRAY TO KEEP TRACK OF WHAT GOT LIT UP
+// on new game button click, start game function()
+/*
+
+start game function(){
+containing the data-quad values for pieces
+Start game function contains FOR LOOP
+var random value 1-4,
+PUSH RANDOM TO ARRAY TO KEEP TRACK OF WHAT HAS BEEN SELECTED
+Calls the light-up function on FOR THAT RANDOM VALUE LIGHT-UP(RANDOM)
+
+light-up function(){
+  FOR LOOP, ITERATING THROUGH AND LIGHTING UP EACH VALUE
+  (WE GET EACH VALUE FROM PREVIOUSLY PUSHING THE RANDOM VALUE INTO THE ARRAY)
+  LIGHT UP THE ARRAY (EVEN IF IT IS JUST THE 1 VALUE)
+  fade in the corresponding data-quad piece FROM VAR
+  do this by adjusting CSS opacity for data-quad piece
+  OR do it by switch/case, use corresponding .class for opacity attr
+  light up for 1-2 seconds (depending on how it looks)
+  FADE OUT DATA-QUAD BACK TO 0.5 OPACITY
+
+}
+
+Record li click after light up function
+get value for li click attr data-quad and compare it to var random
+if recorded click attr data-quad != THE LAST IN THE ARRAY, NOT RANDOM
+(BECAUSE YOU MIGHT END UP MESSING THIS UP OR CALLING ANOTHER RANDOM OR SOMETHING)
+end game function, display round value
+else
+  round ++
+  CONTINUE LOOP
+)
+
+BUT SERIOUSLY, FIGURE OUT HOW TO PUSH THE NEWEST VALUE TO THE ARRAY AND REPEAT
+FROM THE START EACH ITERATION (I THINK I MAY HAVE)
+OH BOY, THAT'S 40 LINES PSUEDOCODE INVOLVING CSS, JS AND JQUERY ON VALUES,
+ARRAYS, AND ELEMENTS THAT YOU DON'T COMPLETELY KNOW HOW TO HANDLE.  GLHF
+*/
+
+var roundNumber=0;
+var lit = [];
+var newGame = $("#newGame");
+  var red = $(".red");
+  var green = $(".green");
+  var blue = $(".blue");
+  var yellow = $(".yellow");
+
+newGame.on("click", function(){
+  lit=[];
+  console.log("New Game!");
+  //while(roundNumber === lit.length){
+  var random = Math.floor(Math.random() * 4) + 1;
+  console.log(random);
+  lit.push(random);
+  console.log(lit);
+  lightUp(random);
+
+//}
+    function lightUp(random){
+      if (random === 1){
+        red.animate({opacity:1.0}, 1500);
+        red.animate({opacity:0.2}, 500);
+        red.on("click", function(){
+          return true;
+
+        });
+      }
+      else if(random ===2){
+        green.animate({opacity:1.0}, 1500);
+        green.animate({opacity:0.2}, 500);
+
+      }
+      else if(random ===3){
+        blue.animate({opacity:1.0}, 1500);
+        blue.animate({opacity:0.2}, 500);
+      }
+      else {
+        yellow.animate({opacity:1.0}, 1500);
+        yellow.animate({opacity:0.2}, 500);
+      }
+    }
+
+
+
+
+
+
+
+});
